2a5561384bb8155a9b6314bef9bc93278b2939a1
polirritmico <ebray187@gmail.com>
2026-09-25 00:55:45 -0300

    feat(main): usa la conexión a la db

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
+import os
+import sys
 
 from src.database import Database
 from src.database_config import DatabaseConfig
+from src.entrada_dao import EntradaDAO
+from src.modo_demo import ModoDemo
 
 
 def main():
-    print("=== BarrioTickera ===")
+    modo_demo = "--demo" in sys.argv
+    print(f"=== BarrioTickera{"-DEMO" if modo_demo else ""} ===")
     db_config = DatabaseConfig()
     database = Database(db_config)
-    database.connect()
+    if not database.connect():
+        os.exit(1)
+
+    with database.conexion:
+        db_version = database.connection.version
+        print(f"Conectado a OracleDB {db_version} como {db_config.service_name}")
+        dao = EntradaDAO(database)
 
 
 if __name__ == "__main__":
