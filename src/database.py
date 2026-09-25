d3137c06886c67ec54fe84b855b661ad7a393a49
polirritmico <ebray187@gmail.com>
2026-09-25 00:56:57 -0300

    feat(Database): implementa listar y agrega try-catch a conexión db

--- src/database.py
+++ src/database.py
@@ -4,12 +4,20 @@ from src.database_config import DatabaseConfig
 
 
 class Database:
-    def __init__(self, config: DatabaseConfig):
-        self.config = config
+    conexion: oracledb.Connection
 
-    def connect(self) -> oracledb.Connection:
-        return oracledb.connect(
-            user=self.config.user,
-            password=self.config.password,
-            dsn=self.config.dsn,
-        )
+    def __init__(self, config: DatabaseConfig):
+        self.config: DatabaseConfig = config
+
+    def connect(self) -> oracledb.Connection | None:
+        try:
+            self.conexion = oracledb.connect(
+                user=self.config.user,
+                password=self.config.password,
+                dsn=self.config.dsn,
+            )
+        except oracledb.DatabaseError as err:
+            print(f"Falló conexión con la base de datos:\n{err}")
+            return None
+
+        return self.conexion
--- src/entrada_dao.py
+++ src/entrada_dao.py
@@ -1,5 +1,6 @@
 from oracledb import Connection, DatabaseError
 
+from src.entrada import Entrada
 from src.exceptions import EntradaError
 
 
@@ -20,3 +21,35 @@ class EntradaDAO:
                 raise EntradaError(err) from err
             else:
                 raise
+
+    def _query(
+        self, procedimiento: str, parametros: list[str | int | None]
+    ) -> list[dict]:
+        with self.conn.cursor() as cur, self.conn.cursor() as ref_cursor:
+            try:
+                cur.callproc(procedimiento, parametros + [ref_cursor])
+            except DatabaseError as err:
+                if EntradaError.es_error_de_negocio(err):
+                    raise EntradaError(err) from err
+                else:
+                    raise
+
+            cols: list = [col[0].lower() for col in ref_cursor.description]
+            return [dict(zip(cols, row)) for row in ref_cursor]
+
+    # def existe(self, id: int) -> bool:
+    #     with self.conn.cursor() as cur:
+    #         return cur.callfunc("pkg_entrada.fn_existe", int, [run]) > 0
+
+    def listar(self, filtro: str | None = None) -> list[dict]:
+        return self._query("pkg_entrada.sp_listar_entrada", [filtro])
+
+    def obtener(self, id: int) -> dict:
+        # TODO: Implementar
+        return self._query("pkg_entrada.sp_listar_entrada", [id])[0]
+
+    def crear(self, entrada: Entrada) -> None:
+        self._execute(
+            "pkg_entrada.sp_insertar_entrada",
+            list(vars(entrada).values()),
+        )
